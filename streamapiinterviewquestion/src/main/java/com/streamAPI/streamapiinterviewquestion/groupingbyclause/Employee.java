6db6b27b5b179bdbbac33f52f8e8db4e4fb4506e
shukl <anandku.shuklasatna@gmail.com>
2024-09-11 01:51:12 +0530

    committed of Stream API coding question for finding the Highest Salary with department name

--- streamapiinterviewquestion/src/main/java/com/streamAPI/streamapiinterviewquestion/groupingbyclause/Employee.java
+++ streamapiinterviewquestion/src/main/java/com/streamAPI/streamapiinterviewquestion/groupingbyclause/Employee.java
@@ -4,6 +4,7 @@ public class Employee {
     private int id;
     private String name;
     private double salary;
+    private  int salary1;
     private String department;
 
     public Employee(int id, String name, double salary) {
@@ -12,10 +13,10 @@ public class Employee {
         this.salary = salary;
     }
 
-    public Employee(int id, String name, double salary, String department) {
+    public Employee(int id, String name, int salary1, String department) {
         this.id = id;
         this.name = name;
-        this.salary = salary;
+        this.salary1 = salary1;
         this.department = department;
     }
 
@@ -36,6 +37,14 @@ public class Employee {
     }
     public void setSalary(double salary) {
         this.salary = salary;
+    }
+
+    public int getSalary1() {
+        return salary1;
+    }
+
+    public void setSalary1(int salary1) {
+        this.salary1 = salary1;
     }
 
     public String getDepartment() {
--- streamapiinterviewquestion/src/main/java/com/streamAPI/streamapiinterviewquestion/groupingbyclause/FindHighestSalaryDepartmentByDepartUsingStreamAPI.java
+++ streamapiinterviewquestion/src/main/java/com/streamAPI/streamapiinterviewquestion/groupingbyclause/FindHighestSalaryDepartmentByDepartUsingStreamAPI.java
@@ -3,23 +3,30 @@ package com.streamAPI.streamapiinterviewquestion.groupingbyclause;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class FindHighestSalaryDepartmentByDepartUsingStreamAPI {
     public static void main(String[] args) {
         List<Employee> employees = Arrays.asList(
-                new Employee(1,"anand",3000.0,"Java Developer"),
-                new Employee(2,"ajaz",3400.0,"Software Engineer"),
-                new Employee(3,"Anmol",3400.0,"Frontend Engineer"),
-                new Employee(4,"Aman",3500.0,"Software Engineer"),
-                new Employee(5,"Akash",3500.0,"Frontend Engineer"),
-                new Employee(6,"Ambuj",4600.00,"Java Developer")
+                new Employee(1,"anand",3000,"Java Developer"),
+                new Employee(2,"ajaz",3400,"Software Engineer"),
+                new Employee(3,"Anmol",3400,"Frontend Engineer"),
+                new Employee(4,"Aman",3500,"Software Engineer"),
+                new Employee(5,"Akash",3500,"Frontend Engineer"),
+                new Employee(6,"Ambuj",4600,"Java Developer")
         );
 
-//        Employee employee = employees.stream().sorted(Comparator.comparingDouble(Employee :: getSalary))
-//                                                  .skip(0).findFirst().get();
-//        System.out.println(employee.getSalary());
 
 
+        Map<String, Employee> map = employees.stream().
+                collect(Collectors.groupingBy(Employee :: getDepartment,
+                        Collectors.collectingAndThen(Collectors.maxBy(Comparator.comparingInt(Employee :: getSalary1)),
+                                emp -> emp.get())));
+
+        for(Map.Entry<String,Employee> entry : map.entrySet()){
+            System.out.println(entry.getKey() + " " + entry.getValue().getSalary1());
+        }
 
     }
 }
